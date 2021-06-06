bbdb597fb0f1a6535cb194955f42e6fb83f165fa
rupesh911 <81091408+rupesh911@users.noreply.github.com>
2021-06-06 14:58:49 +0530

    Done

--- js_projects/script.js
+++ js_projects/script.js
@@ -3,7 +3,12 @@ input1 = document.querySelector("input")
 output = document.querySelector(".output")
 
 button1.addEventListener("click", showMessage)
+
 function showMessage(){
-    let message = `Welcome ${input1.value} in the course of javascript`
-    output.innerHTML =  "<h1> hello </h1>" + message
+    let a 
+    a=((input1.value*10)/100)
+    console.log(a)
+
+    let message = `Tip of ${a} credited in your salary account`
+    output.innerHTML =  message
 }
